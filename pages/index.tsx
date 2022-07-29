1d84c6e64f0b711f1e36cfaba2621fc3e004e839
Ramil Aminov <ramil@aminov.dev>
2022-07-29 12:17:41 +0300

    Personalize

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,56 +14,29 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ramil Aminov
+          Ramil Aminov 👨‍💻
         </h1>
 
         <p className={styles.description}>
-          <code className={styles.code}><a href='https://github.com/ramilaminov' target='_blank' rel='noreferrer'>github.com/ramilaminov</a></code>
+          CTO and Software Engineer
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <a href='https://github.com/ramilaminov' target='_blank' rel='noopener noreferrer' className={styles.card}>
+            <h2>GitHub &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <code className={styles.code}>github.com/ramilaminov</code>
+            </p>
+          </a>
+
+          <a href='https://habr.com/ru/users/ard/' target='_blank' rel='noopener noreferrer' className={styles.card}>
+            <h2>Habr &rarr;</h2>
+            <p>
+              <code className={styles.code}>habr.com/ru/users/ard/</code>
             </p>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
