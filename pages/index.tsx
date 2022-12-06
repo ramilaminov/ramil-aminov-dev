02dd97e6bc786970064a7e69e32e34f9d1ce22e0
Ramil Aminov <ramil@aminov.dev>
2022-12-06 15:34:17 +0300

    Rebuilt using Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,72 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import photo from '../public/photo.png'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex min-h-min sm:min-h-screen flex-col items-center justify-center pt-16 pb-9 sm:pt-2 sm:pb-16">
       <Head>
         <title>Ramil Aminov</title>
         <meta name="description" content="Ramil Aminov" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-6 sm:px-20 text-center">
         <Image
           src={photo}
           alt="Ramil Aminov"
           width={150}
           height={150}
         />
-        
-        <h1 className={styles.title}>
+
+        <h1 className="text-6xl font-bold mt-3">
           Ramil Aminov
         </h1>
 
-        <p className={styles.description}>
-          Software&nbsp;Engineer &amp;&nbsp;CTO
+        <p className="mt-9 text-2xl">
+          Software Engineer, CTO&nbsp;at&nbsp;
+          <a
+            href="https://smarthead.digital"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold hover:underline hover:text-blue-600"
+          >
+            SmartHead
+          </a>
         </p>
 
-        <div className={styles.grid}>
-          <a className={styles.card} href='https://github.com/ramilaminov/' target='_blank' rel='noopener noreferrer'>
-            <code><b>github</b>.com/ramilaminov</code>
-          </a>
-
-          <a className={styles.card} href='https://www.linkedin.com/in/ramilaminov/' target='_blank' rel='noopener noreferrer'>
-            <code><b>linkedin</b>.com/in/ramilaminov</code>
-          </a>
-
-          <a className={styles.card} href='https://dev.to/ramilaminov/' target='_blank' rel='noopener noreferrer'>
-            <code><b>dev.to</b>/ramilaminov</code>
-          </a>
-        </div>
+        <ul className="flex flex-wrap items-center justify-center gap-x-6 gap-y-4 p-3 mt-6 text-xl">
+          <li className="font-mono">
+            <a
+              className="hover:underline hover:text-blue-600"
+              href='https://github.com/ramilaminov/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <b>github</b>.com/ramilaminov
+            </a>
+          </li>
+          <li className="font-mono">
+            <a
+              className="hover:underline hover:text-blue-600"
+              href='https://www.linkedin.com/in/ramilaminov/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <b>linkedin</b>.com/in/ramilaminov
+            </a>
+          </li>
+          <li className="font-mono">
+            <a
+              className="hover:underline hover:text-blue-600"
+              href='https://dev.to/ramilaminov/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <b>dev.to</b>/ramilaminov
+            </a>
+          </li>
+        </ul>
       </main>
     </div>
   )
